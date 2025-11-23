347653f13c6b19022bcfe4a23df9b67f3820706a
Oleksandr Sadovyi <sashsadovyi.000.z@gmail.com>
2025-11-23 11:32:01 +0200

    add all

--- js/task-1.js
+++ js/task-1.js
@@ -1,24 +1,43 @@
-function isEnoughCapacity(products, containerSize) {
-  // Підрахунок загальної кількості товарів
-  const totalQuantity = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
-  
-  // Повертаємо true, якщо все поміщається, або false, якщо ні
-  return totalQuantity <= containerSize;
-}
+// Оголошення функції getUserNames
+const getUserNames = (users) => users.map(user => user.name);
 
-// Перевірка роботи функції
+// Перевірка коректності роботи функції
 console.log(
-  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-); // false
-
-console.log(
-  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
-); // true
-
-console.log(
-  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
-); // false
+  getUserNames([
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      balance: 2811
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      balance: 3821
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      balance: 3793
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      balance: 2278
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      balance: 3951
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      balance: 1498
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      balance: 2764
+    },
+  ])
+); 
